e6bdfacc8303b41c89ca8e7bfc87c9914b6bf60b
smitakanojia <smitakanojia2@gmail.com>
2026-09-21 02:17:12 +0530

    Time: 1 ms (83.82%) | Memory: 42.8 MB (36.24%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> map= new HashMap<>();
+        HashSet<String> seen= new HashSet<>();
+        String[] words = s.split(" ");
+        if(pattern.length()!=words.length) return false;
+        
+        for(int i=0; i<pattern.length(); i++){
+            char c = pattern.charAt(i);
+            String w = words[i];
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(w)){
+                    return false;
+                }
+            }
+            else{
+                if(seen.contains(w)) return false;
+                map.put(c,w);
+                seen.add(w);
+            }
+        }
+        return true;
+    }
+}
